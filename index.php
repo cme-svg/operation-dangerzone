b6c7ac45af05005eca560fb73250cbfe0e6b13fc
Chris Esposito <cme@seventhgeneration.com>
2015-06-17 13:34:55 -0400

    Added colors and excited marks!!!

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
-  print "Hello, world!";
+  echo "Hello, world!";
+echo "
+  <html>
+    <head>
+      <style>
+      body {
+    background-color:blue;}
+      </style>
+      </head>
+      <body>
+        <h1> Hello World!!!! </h1>
+        </body>
+"
 ?>
